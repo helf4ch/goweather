11fa3e0cbeb46d3349983f51923a5509d80b49fa
helf4ch <helf4ch@yandex.ru>
2025-12-22 20:34:34 +0900

    feat: http request for weather

--- src/main.go
+++ src/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
 func main() {
 
+	res, err := http.Get("https://api.open-meteo.com/v1/forecast?latitude=50.2796&longitude=127.5405&current=relative_humidity_2m,temperature_2m&timezone=auto")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(res.StatusCode)
+	fmt.Printf("%s", body)
 }
