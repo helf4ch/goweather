079d6db761f17a5c90b7647dc7fd2771f62123c6
helf4ch <helf4ch@yandex.ru>
2025-12-23 16:32:45 +0900

    feat: change url presentation

--- src/main.go
+++ src/main.go
@@ -6,9 +6,22 @@ import (
 	"net/http"
 )
 
+var (
+	url = "https://api.open-meteo.com/v1/forecast?" +
+		"latitude=50.2796&" +
+		"longitude=127.5405&" +
+		"current=relative_humidity_2m," +
+		"temperature_2m," +
+		"precipitation," +
+		"wind_speed_10m," +
+		"wind_direction_10m," +
+		"wind_gusts_10m," +
+		"surface_pressure&" +
+		"timezone=auto"
+)
 func main() {
 
-	res, err := http.Get("https://api.open-meteo.com/v1/forecast?latitude=50.2796&longitude=127.5405&current=relative_humidity_2m,temperature_2m&timezone=auto")
+	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
 	}
