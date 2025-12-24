85af5c39e51e1692d09afeeb289a43cbabed201c
helf4ch <helf4ch@yandex.ru>
2025-12-24 21:55:51 +0900

    feat: response data parsing

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -58,6 +59,49 @@ func GetWeatherRaw(lat, lon float64) (int, []byte) {
 	return res.StatusCode, body
 }
 
+type Units struct {
+	Humidity      string `json:"relative_humidity_2m"`
+	Temperature   string `json:"temperature_2m"`
+	Precipitation string `json:"precipitation"`
+	Pressure      string `json:"surface_pressure"`
+	WindSpeed     string `json:"wind_speed_10m"`
+	WindDirection string `json:"wind_direction_10m"`
+	WindGusts     string `json:"wind_gusts_10m"`
+}
+
+type Values struct {
+	Time          string  `json:"time"`
+	Humidity      int     `json:"relative_humidity_2m"`
+	Temperature   float64 `json:"temperature_2m"`
+	Precipitation float64 `json:"precipitation"`
+	Pressure      float64 `json:"surface_pressure"`
+	WindSpeed     float64 `json:"wind_speed_10m"`
+	WindDirection int     `json:"wind_direction_10m"`
+	WindGusts     float64 `json:"wind_gusts_10m"`
+}
+
+type Response struct {
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longtitude"`
+	Elevation   float64 `json:"elevation"`
+	Timezone    string  `json:"timezone_abbreviation"`
+	ValuesUnits Units   `json:"current_units"`
+	Values      Values  `json:"current"`
+}
+
+func Parse(c Config, raw []byte) {
+	if c.ShowRaw {
+		fmt.Printf("%s", raw)
+	}
+
+	var data Response
+	if err := json.Unmarshal(raw, &data); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(data)
+}
+
 func main() {
 	showRaw := flag.Bool("raw", false, "show http response body")
 	showTemp := flag.Bool("T", false, "show temperature")
@@ -110,6 +154,6 @@ func main() {
 		return
 	}
 
-	fmt.Println(config)
-	fmt.Println(GetWeatherRaw(lat, lon))
+	_, raw := GetWeatherRaw(lat, lon)
+	Parse(config, raw)
 }
